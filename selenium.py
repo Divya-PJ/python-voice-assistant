d6d229ad59214a4dc1d20c57114a3c337f681e04
Divya-PJ <95292323+Divya-PJ@users.noreply.github.com>
2021-12-16 22:55:52 +0530

    Delete selenium.py

--- selenium.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-
-class infow():
-    def __init__(self):
-        self.driver = webdriver.chrome(executable_path=C:\Users\user\Downloads\chromedriver.exe)
-
-    def get_info(self,query):
-        self.query=query
-        self.driver.get(url="https://www.wikipedia.org")
